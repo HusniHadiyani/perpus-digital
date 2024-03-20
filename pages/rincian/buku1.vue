59c634e586257c8cc3bb38193ff86fed341d9311
HusniHadiyani <142316333+HusniHadiyani@users.noreply.github.com>
2024-03-20 14:46:43 +0700

    Add files via upload

---
+++ pages/rincian/buku1.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="row mt-4 d-flex justify-content-evenly">
+    <div class="col-lg-4">
+      <div class="card">
+        <div class="card-body">
+          <img src="~assets/img/Eccedentesiast.jpg" width="220px" height="340px" class="cover" alt="cover1">
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <h1> NOVEL ECCEDENTESIAST</h1>
+      <p> PENULIS : Ita Krn</p>
+      <p> PENERBIT : Akad</p>
+      <p> TAHUN TERBIT : 27 April 2022</p>
+      <p> JUMLAH HALAMAN : 460</p>
+    </div>
+  
+  <p>Kisah ini menceritakan tentang Aily yang tuna netra dan Canva yang tidak pernah 
+      bahagia. Tentang Canva Narendra, salah satu anggota Diamond Gang yang memiliki 
+      sebuah luka dalam. Cita-citanya hanya satu, bertemu dengan kedua orang tuanya. 
+      Untuk itu, ia belajar dengan giat demi mencapai nilai yang tinggi dan mewujudkan cita-citanya, 
+      ditemani oleh anggota Diamond Gang dan juga seseorang yang ia cintai, Aily. Namun, 
+      kehadiran Zayyan Abrisam, musuh di masa lalu Canva yang memang cerdas 
+      dan selalu mendapatkan peringkat satu membuat hari-hari Canva semakin kacau. 
+      Rasa ambisi untuk menang dan mendapatkan juara satu menyebabkan Canva melupakan kesehatannya. 
+      Apakah Canva mampu untuk mewujudkan mimpinya? Atau, kesehatan lebih dulu merenggut apa yang ia cita-citakan?</p>
+  <nuxt-link to="/buku">
+    <button type="button" class="btn btn-dark mt-5">Kembali</button>
+  </nuxt-link>
+      
+</div>
+</template>
+
+<style scoped>
+.card{
+  width: 255px;
+  height: 370px;
+  box-shadow: 1px 1px 10px #424242;
+}
+</style>
