50e0e92622061a36e27c63be299d1dde1bdf8856
HusniHadiyani <142316333+HusniHadiyani@users.noreply.github.com>
2024-07-22 10:42:57 +0700

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,28 @@
 <template>
-  <div class="row p-3 bg-primary text-white">
+  <div class="row p-3 bg text-white m-0">
     <div class="col-lg-1">
-      <img src="~assets/img/LOGO-SMK4.png" alt="logo SMK 4" width="100">
-    </div>
-    
-    <div class="col-lg-11">
-      <h3>Perpus Digital</h3>
-      <address>
-        SMKN 4 Tasikmalaya<br>JL.Depok, Sukamenak, Purbaratu
-      </address>
+      <img src="@/assets/img/smkn4.png" alt="logo smkn4.png">
+  </div>
+
+<div class="col-lg-11">
+  <h1>perpus Digital</h1>
+  <address>
+    SMKN 4 Tasikmalaya
+    Jl.Depok, Sukamenak, Purbaratu
+    </address>
     </div>
   </div>
 </template>
+<style scoped>
+.bg{
+  background-color:#414552;
+  box-shadow:#414552;
+  width: 100%;
+  left: 0;
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+
+img{
+  width: 100px;
+}
+</style>
