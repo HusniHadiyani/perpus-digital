aaa1414b80f72854b84cc657410058d217d0c04d
HusniHadiyani <142316333+HusniHadiyani@users.noreply.github.com>
2024-07-22 13:06:34 +0700

    Update [id].vue

--- pages/buku/[id].vue
+++ pages/buku/[id].vue
@@ -1,53 +1,64 @@
 <template>
-  <div>
-      <h2 class="text-start my-4">{{ buku.judul }}</h2>
-      <div class="row">
-          <div class="col-md-3">
-              <div class="card">
-                  <div class="card-body">
-                      <span v-if="buku.cover"><img class="cover" :src="buku.cover" :alt="buku.judul"></span>
-              <span v-else><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F508699%2Flandscape-placeholder&psig=AOvVaw2-SWmfk33NzXubPfqn0P16&ust=1714794757874000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjln7nK8IUDFQAAAAAdAAAAABAE://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F508699%2Flandscape-placeholder&psig=AOvVaw2-SWmfk33NzXubPfqn0P16&ust=1714794757874000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjln7nK8IUDFQAAAAAdAAAAABAE" class="cover"></span>
-                  </div>
-              </div>
-          </div>
-          <div class="col-md-6">
-              <ul class="list-group list-group-flush">
-                  <li class="list-group-item">penulis : {{  buku.penulis }}</li>
-                  <li class="list-group-item">tahun_terbit : {{  buku.tahun_terbit }}</li>
-                  <li class="list-group-item">rak : {{ buku.rak }}</li>
-                  <li class=" list-group-item">deskripsi : {{ buku.deskripsi }}</li>
-              </ul>
-          </div>
+  <div class="bg">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="icon mt-4">
+          <nuxt-link to="/Buku/"> </nuxt-link>
+          <h2 class="text-start text-white my-4 text-center">{{ buku.judul }}</h2>
+        </div>
       </div>
-      <NuxtLink to="/buku" class="btn btn-light btn-lg rounded-5 px-5">
-      kembali
-      </NuxtLink>
+    </div>
+    <div class="row">
+      <div class="col-4 text-white d-flex flex-column justify-contant-center">
+        <h4>detail buku:</h4>
+        <h5>penulis: {{ buku.penulis }}</h5>
+        <h5>jumlah Halaman: {{ buku.jumlah_halaman }}</h5>
+        <h5>tahun terbit: {{ buku.tahun_terbit }}</h5>
+        <h5>penerbit: {{ buku.penerbit }}</h5>
+      </div>
+      <div class="col-4 d-flex flex-column justify-content-center align-items">
+        <div class="mb-5">
+          <div class="card-body">
+            <img :src="buku.cover" class="ukuran center" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="row text-white">
+      <div class="col">
+        <h4 class="text-white">Deskripsi</h4>
+        <h5>{{ buku.deskripsi }}</h5>
+      </div>
+    </div>
+    <NuxtLink to="/Buku/">
+      <button type="submit" class="btn btn-dark btn-lg rounded-5 px-5">kembali</button>
+    </NuxtLink>
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+const supabase = useSupabaseClient();
 
-const supabase = useSupabaseClient()
-const route = useRoute()
-const buku = ref([])
+const route = useRoute();
 
-const getBukuByID = async () => {
-  const { data, error } = await supabase
-  .from ('buku')
-  .select(`*, kategori_buku(*)`)
-  .eq('id', route.params.id)
-  .single()
-  if(data) buku.value = data
+const buku = ref([]);
 
-}
+const getBooksById = async () => {
+  const { data, error } = await supabase.from("buku").select(`*, kategori(*)`).eq("id", route.params.id);
+  if (data) buku.value = data[0];
+};
 
 onMounted(() => {
-  getBukuByID()
-})
+  getBooksById();
+});
 </script>
+
 <style scoped>
-.cover{
-  width: 100%;
+.bg {
+  background-image: url(assets/img/yy.webp);
 }
-</style>
+.ukuran {
+  width: 180px;
+  display: flex;
+}
+</style>              
