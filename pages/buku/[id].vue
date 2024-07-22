3062dde05b22aedf25fb8ad4d8ec0f7af6e78626
HusniHadiyani <142316333+HusniHadiyani@users.noreply.github.com>
2024-07-22 10:44:13 +0700

    Update [id].vue

--- pages/buku/[id].vue
+++ pages/buku/[id].vue
@@ -1,50 +1,53 @@
 <template>
-  <div class="container-fluid">
-    <div class="row mt-4 d-flex justify-content-evenly">
-      <div class="col-lg-4">
-        <div class="card">
-          <div class="card-body">
-            <img src="~/assets/img/cover1.jpg" width="220px" height="340px" class="cover" alt="cover1">
+  <div>
+      <h2 class="text-start my-4">{{ buku.judul }}</h2>
+      <div class="row">
+          <div class="col-md-3">
+              <div class="card">
+                  <div class="card-body">
+                      <span v-if="buku.cover"><img class="cover" :src="buku.cover" :alt="buku.judul"></span>
+              <span v-else><img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F508699%2Flandscape-placeholder&psig=AOvVaw2-SWmfk33NzXubPfqn0P16&ust=1714794757874000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjln7nK8IUDFQAAAAAdAAAAABAE://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.svgrepo.com%2Fsvg%2F508699%2Flandscape-placeholder&psig=AOvVaw2-SWmfk33NzXubPfqn0P16&ust=1714794757874000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCNjln7nK8IUDFQAAAAAdAAAAABAE" class="cover"></span>
+                  </div>
+              </div>
           </div>
-        </div>
+          <div class="col-md-6">
+              <ul class="list-group list-group-flush">
+                  <li class="list-group-item">penulis : {{  buku.penulis }}</li>
+                  <li class="list-group-item">tahun_terbit : {{  buku.tahun_terbit }}</li>
+                  <li class="list-group-item">rak : {{ buku.rak }}</li>
+                  <li class=" list-group-item">deskripsi : {{ buku.deskripsi }}</li>
+              </ul>
+          </div>
       </div>
-      <div class="col-4">
-        <h5>Penulis: {{ Buku.penulis }}</h5>
-        <h5>Penerbit: {{ Buku.penerbit }}</h5>
-        <h5>Tahun terbit: {{ Buku.tahun_terbit }}</h5>
-      </div>
-    </div>
-    <div class="row mt-5">
-      <h2>Sinopsis</h2>
-      <p class="mt-3"> Kisah ini menceritakan tentang Aily yang tuna netra dan Canva yang tidak pernah bahagia. Tentang Canva Narendra, salah satu anggota Diamond Gang yang memiliki sebuah luka dalam. Cita-citanya hanya satu, bertemu dengan kedua orang tuanya. Untuk itu, ia belajar dengan giat demi mencapai nilai yang tinggi dan mewujudkan cita-citanya, ditemani oleh anggota Diamond Gang dan juga seseorang yang ia cintai, Aily. Namun, kehadiran Zayyan Abrisam, musuh di masa lalu Canva yang memang cerdas dan selalu mendapatkan peringkat satu membuat hari-hari Canva semakin kacau. Rasa ambisi untuk menang dan mendapatkan juara satu menyebabkan Canva melupakan kesehatannya. Apakah Canva mampu untuk mewujudkan mimpinya? Atau, kesehatan lebih dulu merenggut apa yang ia citakan?</p>
-    </div>
-    <nuxt-link to="/buku">
-      <button type="button" class="btn btn-dark mt-5">Kembali</button>
-    </nuxt-link>
+      <NuxtLink to="/buku" class="btn btn-light btn-lg rounded-5 px-5">
+      kembali
+      </NuxtLink>
   </div>
 </template>
 
-<style scoped>
-.card {
-  width: 255px;
-  height: 370px;
-  box-shadow: 1px 1px 10px #424242;
-}
-</style>
-
 <script setup>
-const supabase = useSupabaseClient()
+import { onMounted } from 'vue';
 
+const supabase = useSupabaseClient()
 const route = useRoute()
 const buku = ref([])
 
-const getBookById = async () => {
-  const { data, error } = await supabase.from('buku').select(`*, kategori(*)`)
+const getBukuByID = async () => {
+  const { data, error } = await supabase
+  .from ('buku')
+  .select(`*, kategori_buku(*)`)
   .eq('id', route.params.id)
-  if(data) buku.value = data[0]
+  .single()
+  if(data) buku.value = data
+
 }
 
 onMounted(() => {
-  getBookById()
+  getBukuByID()
 })
 </script>
+<style scoped>
+.cover{
+  width: 100%;
+}
+</style>
