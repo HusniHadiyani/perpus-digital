1cec3bc87741782cae172655c24530c5658ed009
HusniHadiyani <142316333+HusniHadiyani@users.noreply.github.com>
2024-07-22 10:45:24 +0700

    Update index.vue

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -1,55 +1,59 @@
 <template>
-  <div class="container-fluid">
+  <div class="content">
+    <div class="container-fluid">
       <div class="row">
-          <div class="col-lg-12">
-              <h2 class="text-center my-4">BUKU</h2>
-              <div class="my-3">
-               <input type="search" class="form-control rounded-5" placeholder="Mau baca apa hari ini?">
+        <div class="col-lg-12">
+          <nuxt-link to="/pengunjung">
+            <button type="button" class="btn btn-outline-dark mt-4 btn-lg">KEMBALI</button>
+          </nuxt-link>
+            <h2 class="text-center my-4 fw-bold">RAK BUKU</h2>
+            <form @submit.prevent="getBooks" class="col mb-3">
+              <div class="input-group flex-nowrap rounded">
+                <input v-model="keyword" type="search" class="form-control from-control-lg rounded-5" placeholder="Cari Judul..." aria-label="Search" @input="getbooks" />
               </div>
-               <div class="my-3 text-muted">menampilkan 3 dari 3
-              </div>
-              <div class="row">
-                  <div class="col-lg-2">
-                      <div class="card mb-3">
-                          <div class="card-body">
-                            <nuxt-link to="/rincian/buku1">
-                              <img src="~assets/img/Eccedentesiast.jpg" class="cover" alt="cover1">
-                            </nuxt-link>
-                          </div>
+            </form>
+            <div class="my-3">Menampilkan {{ books.length }} buku dari {{ jmlbuku }}</div>
+          </div>
+        </div>
+          <div class="row shadow-lg">
+              <div v-for="(book,i) in books" :key="i" class="col">
+                  <div class="card mb-3 col-lg-2 col-2">
+                    <nuxt-link :to="`/buku/${book.id}`">
+                      <div class="card-body">
+                        <img :src="book.cover" class="cover" alt="cover">
                       </div>
+                    </nuxt-link>
                   </div>
-                  <div class="col-lg-2">
-                      <div class="card mb-3">
-                          <div class="card-body">
-                              <img src="~assets/img/B.INDO.jpg" class="cover" alt="cover2">
-                          </div>
-                      </div>
-                  </div>
-                  <div class="col-lg-2">
-                      <div class="card mb-3">
-                          <div class="card-body">
-                              <img src="~assets/img/sejarah.jpg" class="cover" alt="cover3">
-                          </div>
-                      </div>
-                  </div>    
               </div>
           </div>
-          <nuxt-link to="/buku"></nuxt-link>
-          <button type="button" class="btn btn-dark mt-5">Kembali</button>
-      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
-.card-body {
-  width: 100%;
-  height: 20em;
-  padding: 0;
+<script setup>
+const supabase= useSupabaseClient()
+
+const keyword = ref('')
+const books = ref([])
+const jmlbuku = ref(0)
+
+const getbooks = async () => {
+  const { data ,error } = await supabase
+  .from('buku')
+  .select(`*, kategori(*)`)
+  .ilike("judul", `%${keyword.value}%`)
+  .order('id')
+  if(data) books.value = data
 }
-.cover {
-  widows: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: 0 30;
+const getJmlBuku = async () => {
+  const{ data, count } = await supabase
+    .from("buku") 
+    .select('*', { count: "exact" })
+    if(data) jmlbuku.value = count
 }
-</style>
+
+onMounted(() =>{
+  getbooks()
+  getJmlBuku()
+
+})
